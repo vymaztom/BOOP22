1ca3863c67e8f1444dff786140a762f9383aac20
Mastný Gabriel <mastny.gabriel@gmail.com>
2020-02-29 16:49:19 +0100

    Project creation

---
+++ GraphCalculator/GraphCalculator/View/MainView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace GraphCalculator.View
+{
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
